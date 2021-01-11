9b7725565e9eef3dfa58c7887100a0376157f47e
Helmi Hartanto <>
2021-01-11 10:16:12 +0700

    First Commit

---
+++ src/com/example/lambda/A07DetermineNot.java
@@ -0,0 +1,24 @@
+package com.example.lambda;
+
+import java.util.List;
+import java.util.Optional;
+
+public class A07DetermineNot {
+    
+    public static void main(String[] args) {
+        
+        List<Employee> eList = Employee.createShortList();
+        
+        Optional<Employee> e1 = eList.stream()
+            .filter(e -> e.getRole().equals(Role.EXECUTIVE))
+            .sequential().findAny();
+        
+        Optional<Employee> e2 = eList.stream()
+            .filter(e -> e.getRole().equals(Role.EXECUTIVE))
+            .parallel().findAny();
+        
+        System.out.println("The same: " + 
+            e1.get().getEmail().equals(e2.get().getEmail()));
+        
+    }
+}
---
+++ src/com/example/lambda/Role.java
@@ -0,0 +1,3 @@
+package com.example.lambda;
+
+public enum Role { STAFF, MANAGER, EXECUTIVE }
